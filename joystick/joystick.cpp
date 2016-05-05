9b6ea6fe881e05afb5ae4b983ff7086bf72e109c
chrolss <chrolss@kth.se>
2016-05-05 12:28:14 +0200

    added joystick structure

---
+++ joystick/joystick.cpp
@@ -0,0 +1,53 @@
+#include "joystick.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "unistd.h"
+
+Joystick::Joystick()
+{
+  openPath("/dev/input/js0");
+}
+
+Joystick::Joystick(int joystickNumber)
+{
+  std::stringstream sstm;
+  sstm << "/dev/input/js" << joystickNumber;
+  openPath(sstm.str());
+}
+
+Joystick::Joystick(std::string devicePath)
+{
+  openPath(devicePath);
+}
+
+void Joystick::openPath(std::string devicePath)
+{
+  _fd = open(devicePath.c_str(), O_RDONLY | O_NONBLOCK);
+}
+
+bool Joystick::sample(JoystickEvent* event)
+{
+  int bytes = read(_fd, event, sizeof(*event));
+
+  if (bytes == -1)
+    return false;
+
+  // NOTE if this condition is not met, we're probably out of sync and this
+  // Joystick instance is likely unusable
+  return bytes == sizeof(*event);
+}
+
+bool Joystick::isFound()
+{
+  return _fd >= 0;
+}
+
+Joystick::~Joystick()
+{
+  close(_fd);
+};
