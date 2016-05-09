ce11543a25577c1da96a6fc79d637d1ff46c37ab
chrolss <chrolss@kth.se>
2016-05-09 20:49:47 +0200

    wtf

--- src/com.cpp
+++ src/com.cpp
@@ -1,4 +1,5 @@
 #include "com.h"
+#include <sstream>
 
 com::com(){
 	initialize();
@@ -37,12 +38,12 @@ void com::initialize(){
 
 void com::readMsg(){
 	bzero(buffer,256);
-	double msgBuff[256];
-	bzero(msgBuff,256);
-	n = read(newsockfd,msgBuff,255);
+	n = read(newsockfd,buffer,255);
+	double m = atof(buffer);
 	if (n < 0) error("ERROR reading from socket");
-	printf("Here is the message: %f \n",msgBuff[0]);
-	printf("Here is the second msg: %f \n", msgBuff[1]);
+	printf("Here is the message: %f \n",m);
+	std::cout << m << std::endl;
+	//printf("Here is the second msg: %f \n", atof(buffer[1]));
 	//break communication if client sends "quit"
 	std::string str(buffer);
 	std::string subst = str.substr(0,4);
